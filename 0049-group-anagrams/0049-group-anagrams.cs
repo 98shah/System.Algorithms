f3931c968831ac52f9b02c09ce036cb533a9b367
Hardik <23shahardik@gmail.com>
2026-04-24 14:54:11 +0530

    Time: 18 ms (85.67%), Space: 67.4 MB (40.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cs
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -1,22 +1,21 @@
 public class Solution {
     public IList<IList<string>> GroupAnagrams(string[] strs) {
-       Dictionary<string, List<string>> groups = [];
+        Dictionary<string, List<string>> groups = [];
+        
+        foreach (string s in strs){
+            int[] count = new int[26];
 
-       foreach (string s in strs){
-        int[] count = new int[26];
+            foreach (char c in s){
+                count[c - 'a']++;
+            }
 
-        foreach (char c in s){
-            count[c - 'a']++;
+            string key = string.Join(",",count);
+
+            if(!groups.ContainsKey(key)){
+                groups.Add(key, []);
+            }
+            groups[key].Add(s);
         }
-
-        string key = string.Join(",", count);
-
-        if (!groups.ContainsKey(key)){
-            groups.Add(key, []);
-        }
-        groups[key].Add(s);
-       } 
-
-       return [.. groups.Values];
+        return [.. groups.Values];
     }
 }
