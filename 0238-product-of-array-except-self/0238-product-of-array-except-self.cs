c686c0afa04ae28e0481616dcd7a9625b8629412
Hardik <23shahardik@gmail.com>
2026-05-11 12:34:01 +0530

    Time: 1 ms (100%), Space: 62.9 MB (67.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,18 +1,15 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        int n = nums.Length;
-        int[] answer = new int[n];
-
-        answer[0] = 1;
-        for (int i=1; i<n; i++){
-            answer[i] = answer[i-1] * nums[i-1];
+        int[] r= new int[nums.Length];
+        r[0]=1;
+        for(int i=1;i<nums.Length;i++){
+            r[i] = r[i-1]*nums[i-1];
         }
-
-        int rp = 1;
-        for (int i=n-1; i >= 0; i--){
-            answer[i] = answer[i] * rp;
-            rp = rp * nums[i];
+        int rp=1;
+        for(int j=nums.Length-1;j>=0;j--){
+            r[j] *=rp;
+            rp *=nums[j];
         }
-        return answer;
+        return r;
     }
-}
+} 
