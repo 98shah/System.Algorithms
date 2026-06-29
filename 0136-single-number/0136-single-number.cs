cb5d61d175793c52f11330f685f8ce1a15e8d889
Hardik <23shahardik@gmail.com>
2026-06-29 23:21:55 +0530

    Time: 0 ms (100%), Space: 46.8 MB (82.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach(var n in nums){
+            result ^= n;
+        }
+        return result;
+    }
+}
