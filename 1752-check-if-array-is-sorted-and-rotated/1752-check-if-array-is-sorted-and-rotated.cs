197626e55739fcee19cd13534f3a593d81344323
Hardik <23shahardik@gmail.com>
2026-07-27 11:10:56 +0530

    Time: 0 ms (100%), Space: 43.4 MB (25%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
@@ -1,12 +1,9 @@
 public class Solution {
     public bool Check(int[] nums) {
-        int n = nums.Length;
-        int flag = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i] > nums[(i+1) % n]) flag++;
-            if (flag > 1) return false;
+        int spikeDrops = 0;
+        for(int i=0; i<nums.Length; i++){
+            if(nums[i] > nums[(i+1) % nums.Length] && ++spikeDrops > 1) return false;
         }
         return true;
-
     }
 }
