3dcedd111d3585413e9da833b32a673a931ac8fa
Hardik <23shahardik@gmail.com>
2026-07-31 22:51:32 +0530

    Time: 1 ms (97.33%), Space: 58.1 MB (61.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
         int n = nums.Length;
-        int zp = 0;
-        for(int i=0; i<n; i++){
+        int zp=0;
+        for(int i=0; i < n; i++){
             if(nums[i] != 0){
-                int temp = nums[zp];
-                nums[zp] = nums[i];
-                nums[i] = temp;
+                (nums[zp], nums[i]) = (nums[i], nums[zp]);
                 zp++;
             }
         }
