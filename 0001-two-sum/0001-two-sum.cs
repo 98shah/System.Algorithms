67068e70d066d5b37c0ce469c66e4fe80b671c9f
Hardik <23shahardik@gmail.com>
2026-07-31 23:32:23 +0530

    Time: 4 ms (88.2%), Space: 50.1 MB (20.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         int n = nums.Length;
-        Dictionary<int, int> map = new Dictionary<int, int>();
+        Dictionary<int, int> map = [];
         for(int i=0; i<n; i++){
             int diff = target - nums[i];
             if(map.ContainsKey(diff)){
                 return [i, map[diff]];
             }
-
             map[nums[i]] = i;
         }
         return [-1, -1];
