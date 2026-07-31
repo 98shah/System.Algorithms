b7a475e56b72030fbc55c45592458921258683db
Hardik <23shahardik@gmail.com>
2026-07-31 23:26:34 +0530

    Time: 2 ms (13.08%), Space: 50 MB (97.42%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cs
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cs
@@ -1,20 +1,15 @@
-using System.Linq;
 public class Solution {
     public int FindMaxConsecutiveOnes(int[] nums) {
         int n = nums.Length;
-        int i = 0;
         int max_count = 0;
-        while(i < n){
+        int count = 0;
+        for(int i=0; i<n; i++){
             if(nums[i] == 1){
-                int count = 0;
-                while(nums[i] == 1){
-                    count++;
-                    i++;
-                    if(i == n) break;
-                }
+                count++;
                 max_count = Math.Max(count, max_count);
+            } else {
+                count = 0;
             }
-            i++;
         }
         return max_count;
     }
